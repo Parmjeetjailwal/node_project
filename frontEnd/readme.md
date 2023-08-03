d6a59c34bcebf35e832a1ba1301f192406e432f6
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2023-08-03 10:21:05 +0530

    add jenkins file with helm

--- frontEnd/readme.md
+++
@@ -1,3 +0,0 @@
-# Readme
-
-## Change the backend connection URL where the backend server will run (check the routes.js)
--- mysql/mysql_script.sql
+++
@@ -1,3 +0,0 @@
--- CREATE DATABASE employees_db;
--- USE employees_db;
-CREATE TABLE IF NOT EXISTS EMPLOYEES ( emp_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, emp_name varchar(255) NOT NULL, emp_contact varchar(10), emp_add varchar(255) DEFAULT false ) ENGINE=InnoDB DEFAULT CHARSET=utf8;  
